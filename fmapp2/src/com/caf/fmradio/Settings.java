CyanogenMod/android_hardware_qcom_fm

FM: always use overlay default country
Change-Id: I2acbbde684e7581124577f6e929397279d97cb41

fmapp2/src/com/caf/fmradio/Settings.java
@@ -479,8 +479,7 @@ public class Settings extends PreferenceActivity implements
                  mBandPreference
                         .setValueIndex(FmSharedPreferences.REGIONAL_BAND_CHINA);
              }else {
-                 mBandPreference
-                        .setValueIndex(FmSharedPreferences.REGIONAL_BAND_NORTH_AMERICA);
+                 mBandPreference.setValueIndex(FmSharedPreferences.mDefCountryIndex);
              }
              if (mRxMode) {
                 mAudioPreference.setValueIndex(0);
@@ -494,8 +493,7 @@ public class Settings extends PreferenceActivity implements
                     FmSharedPreferences
                     .setCountry(FmSharedPreferences.REGIONAL_BAND_CHINA);
                 }else{
-                    FmSharedPreferences
-                    .setCountry(FmSharedPreferences.REGIONAL_BAND_NORTH_AMERICA);
+                    FmSharedPreferences.setCountry(FmSharedPreferences.mDefCountryIndex);
                 }
              }
              mPrefs.Save();

SlimRoms/hardware_qcom_fm

(cherry picked from commit 087fb5a9128f8a9d96289815b1ae4be2ea98981a)

fmapp2/src/com/caf/fmradio/Settings.java
@@ -479,8 +479,7 @@ public class Settings extends PreferenceActivity implements
                  mBandPreference
                         .setValueIndex(FmSharedPreferences.REGIONAL_BAND_CHINA);
              }else {
-                 mBandPreference
-                        .setValueIndex(FmSharedPreferences.REGIONAL_BAND_NORTH_AMERICA);
+                 mBandPreference.setValueIndex(FmSharedPreferences.mDefaultCountryIndex);
              }
              if (mRxMode) {
                 mAudioPreference.setValueIndex(0);
@@ -494,8 +493,7 @@ public class Settings extends PreferenceActivity implements
                     FmSharedPreferences
                     .setCountry(FmSharedPreferences.REGIONAL_BAND_CHINA);
                 }else{
-                    FmSharedPreferences
-                    .setCountry(FmSharedPreferences.REGIONAL_BAND_NORTH_AMERICA);
+                    FmSharedPreferences.setCountry(FmSharedPreferences.mDefaultCountryIndex);
                 }
              }
              mPrefs.Save();